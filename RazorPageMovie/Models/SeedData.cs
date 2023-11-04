9ed931573b85cb181e12702aa224cbfe8cdf88f8
Miriam Ramírez <147944868+MiriamERamirez@users.noreply.github.com>
2023-11-04 10:02:07 -0600

    Agregar archivos de proyecto.

---
+++ RazorPageMovie/Models/SeedData.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPageMovie.Data;
+
+namespace RazorPageMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize (IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPageMovieContext1(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<RazorPageMovieContext1>>()))
+            {
+                //En caso de contexto nulo, se envia un mensaje de error
+                if (context == null || context.Movie == null)
+                {
+                    throw new ArgumentNullException("Null RazorPagesMovieContext");
+                }
+
+                if (context.Movie.Any())
+                {
+                    //Base de datos muestra todo lo que contiene esta clase
+                    return;
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "When Harry met Sally",
+                        RealeseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Romantic Comedy",
+                        Price = 7.99M,
+                        Rating = "R"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ghostbusters",
+                        RealeseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Price = 8.99M,
+                        Rating = "G"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ghostbusters 2",
+                        RealeseDate = DateTime.Parse("1989-2-23"),
+                        Genre = "Comedy",
+                        Price = 9.99M,
+                        Rating = "G"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Rio Bravo",
+                        RealeseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Western",
+                        Price = 3.99M,
+                        Rating = "NA"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
